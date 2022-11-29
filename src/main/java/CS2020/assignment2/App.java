923540ae027fb97e10681c03f50b788537b64196
Osman Elyas <osmanelyas03@gmail.com>
2022-11-29 20:22:43 +0000

    Set up panels

--- src/main/java/CS2020/assignment2/App.java
+++ src/main/java/CS2020/assignment2/App.java
@@ -1,4 +1,7 @@
 package CS2020.assignment2;
+import java.awt.Color;
+
+
 import javax.swing.*;
 
 
@@ -7,23 +10,34 @@ import javax.swing.*;
  *
  */
 public class App extends JFrame {
+    
+    JMenuBar menuBar;
+    JMenu actionsMenu;
+    JMenuItem about;
+    JMenuItem importData;
+    JMenuItem inventory;
+    JMenuItem export;
+    JPanel mainPanel;
+    JPanel  rightPanel;
+    JPanel bottomPanel;
+
     JTextField productId;
-    JPanel mainPane;
 
     App(){
-
+        // Create the main frame that will be used
         this.setTitle("<Osman Elyas> : Assignment 2");
         this.setSize(800,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JMenuBar menuBar = new JMenuBar();
+        //implement the menu bar
+        menuBar= new JMenuBar();
 
-        JMenu actionsMenu = new JMenu("Actions");
+        actionsMenu = new JMenu("Actions");
 
-        JMenuItem about = new JMenuItem("About");
-        JMenuItem importData = new JMenuItem("Import Data");
-        JMenuItem inventory = new JMenuItem("Inventory");
-        JMenuItem export = new JMenuItem("Export to CSV");
+        about = new JMenuItem("About");
+        importData = new JMenuItem("Import Data");
+        inventory = new JMenuItem("Inventory");
+        export = new JMenuItem("Export to CSV");
 
         actionsMenu.add(about);
         actionsMenu.add(importData);
@@ -34,12 +48,26 @@ public class App extends JFrame {
         this.setJMenuBar(menuBar);
         
 
+        //seperate the frame into the needed panels
+        mainPanel = new JPanel();
+        mainPanel.setBackground(Color.red);
+        mainPanel.setBounds(0,0,550, 450);
+
+        rightPanel = new JPanel();
+        rightPanel.setBackground(Color.green);
+        rightPanel.setBounds(550,0,250, 450);
+
+        bottomPanel = new JPanel();
+        bottomPanel.setBackground(Color.blue);
+        bottomPanel.setBounds(50,100,800, 100);
+
+        this.add(mainPanel);
+        this.add(rightPanel);
+        this.add(bottomPanel);
+
+
         this.setVisible(true);     //Make frame visible
 
-        
-         
-
-        
     }
 
     public class ProductListGUI{
