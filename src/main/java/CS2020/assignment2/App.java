e08ed664f05590bd2a193b668d92b7fe9dc43fd2
Osman Elyas <osmanelyas03@gmail.com>
2022-11-29 14:54:06 +0000

    adding text attempt

--- src/main/java/CS2020/assignment2/App.java
+++ src/main/java/CS2020/assignment2/App.java
@@ -6,19 +6,37 @@ import javax.swing.*;
  * Hello world!
  *
  */
-public class App {
+public class App extends JFrame {
+    JTextField productId;
+    JPanel mainPane;
+
+    App(){
+
+        this.setTitle("<Osman Elyas> : Assignment 2");
+        this.setSize(800,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        productId = new JTextField("Product Id");
+        this.add(productId);
+        this.getContentPane().add(yellowLabel, BorderLayout.CENTER);
+
+        this.setVisible(true);     //Make frame visible
+
+        
+         
+
+        
+    }
+
+    public class ProductListGUI{
+        
+    }
 
 
     
     public static void main( String[] args )
     {
-        JFrame frame = new JFrame();
-        frame.setTitle("<Osman Elyas> : Aassignment 2");
-        frame.setSize(800,600);
+        new App();
 
-        
-        frame.setVisible(true);     //Make frame visible
-   
     }
         
         
