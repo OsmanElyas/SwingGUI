2d5798252a95609b62ae212855d0c03707399ae8
Osman Elyas <osmanelyas03@gmail.com>
2022-11-29 15:09:42 +0000

    Menu Bar Added

--- src/main/java/CS2020/assignment2/App.java
+++ src/main/java/CS2020/assignment2/App.java
@@ -15,9 +15,24 @@ public class App extends JFrame {
         this.setTitle("<Osman Elyas> : Assignment 2");
         this.setSize(800,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        productId = new JTextField("Product Id");
-        this.add(productId);
-        this.getContentPane().add(yellowLabel, BorderLayout.CENTER);
+
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu actionsMenu = new JMenu("Actions");
+
+        JMenuItem about = new JMenuItem("About");
+        JMenuItem importData = new JMenuItem("Import Data");
+        JMenuItem inventory = new JMenuItem("Inventory");
+        JMenuItem export = new JMenuItem("Export to CSV");
+
+        actionsMenu.add(about);
+        actionsMenu.add(importData);
+        actionsMenu.add(inventory);
+        actionsMenu.add(export);
+
+        menuBar.add(actionsMenu);
+        this.setJMenuBar(menuBar);
+        
 
         this.setVisible(true);     //Make frame visible
 
