312f45254a57c5317ad9873cde02c5a1ad1baed7
Osman Elyas <osmanelyas03@gmail.com>
2022-11-29 13:48:12 +0000

    Initial commit

---
+++ src/main/java/CS2020/assignment2/App.java
@@ -0,0 +1,25 @@
+package CS2020.assignment2;
+import javax.swing.*;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+
+    
+    public static void main( String[] args )
+    {
+        JFrame frame = new JFrame();
+        frame.setTitle("<Osman Elyas> : Aassignment 2");
+        frame.setSize(800,600);
+
+        
+        frame.setVisible(true);     //Make frame visible
+   
+    }
+        
+        
+}
