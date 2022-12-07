3871af45e5368ea82a077584879432b7c8c78fb8
Osman Elyas <osmanelyas03@gmail.com>
2022-12-07 23:14:38 +0000

    Created product.java

--- src/main/java/CS2020/assignment2/App.java
+++ src/main/java/CS2020/assignment2/App.java
@@ -1,6 +1,7 @@
 package CS2020.assignment2;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 
 /**
@@ -9,26 +10,30 @@ import javax.swing.*;
  */
 public class App extends JFrame {
     
-    JMenuBar menuBar;
-    JMenu actionsMenu;
-    JMenuItem about;
-    JMenuItem importData;
-    JMenuItem inventory;
-    JMenuItem export;
-    JPanel mainPanel;
-    JPanel  rightPanel;
-    JPanel bottomPanel;
 
-    JButton newItem;
-    JButton save;
-    JButton delete;
-    JScrollPane scroll;
 
-    App(){
+    public static void ProductListGUI(){
+
+        JMenuBar menuBar;
+        JMenu actionsMenu;
+        JMenuItem about;
+        JMenuItem importData;
+        JMenuItem inventory;
+        JMenuItem export;
+        JPanel mainPanel;
+        JPanel  rightPanel;
+        JPanel bottomPanel;
+    
+        JButton newItem;
+        JButton save;
+        JButton delete;
+        JScrollPane scroll;
+
+        JFrame frame = new JFrame();
         // Create the main frame that will be used
-        this.setTitle("<Osman Elyas> : Assignment 2");
-        this.setSize(800,600);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("<Osman Elyas> : Assignment 2");
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
 
         //implement the menu bar
@@ -47,7 +52,7 @@ public class App extends JFrame {
         actionsMenu.add(export);
 
         menuBar.add(actionsMenu);
-        this.setJMenuBar(menuBar);
+        frame.setJMenuBar(menuBar);
         
 
         //seperate the frame into the needed panels
@@ -55,33 +60,44 @@ public class App extends JFrame {
         rightPanel = new JPanel();
         bottomPanel = new JPanel();
 
-        mainPanel.setBackground(Color.red);
-        rightPanel.setBackground(Color.green);
-        bottomPanel.setBackground(Color.blue);
+        //create and add border title to main panel
+        Border blackline = BorderFactory.createTitledBorder("Product Details");
+        mainPanel.setBorder(blackline);
 
+
+        //Create the panels to seperate the GUI into different sections
         mainPanel.setPreferredSize(new Dimension(550, 450));
         rightPanel.setPreferredSize(new Dimension(250, 450));
         bottomPanel.setPreferredSize(new Dimension(800, 100));
 
-        //Product details section in left hand side
-        //mainPanel.setLayout(new GridLayout());
-        JLabel label = new JLabel("Product Id");
-        JTextField product = new JTextField("8101ac25-fcf1-4c32-a3f1-2a946");
-        JLabel label2 = new JLabel("Name");
+
+        //Define the Product details section in left hand side 
+
         
 
+        JLabel label = new JLabel("Product Id");
+        JTextField product = new JTextField("8101ac25-fcf1-4c32-a3f1-2a946");
+        
         JLabel label3 = new JLabel("Item Type");
         String[] items = { "Select type", "Homeware", "Hobby", "Garden" };
         JComboBox<String> itemList = new JComboBox<>(items);
         itemList.setSelectedIndex(0);
-        JTextField name = new JTextField(20);        
+
+        JLabel label2 = new JLabel("Name");
+        
+        JTextField name = new JTextField(20); 
+        
+
         JLabel label4 = new JLabel("Quantity");
         JTextField quantity = new JTextField(5);
+
         JCheckBox delivery = new JCheckBox("Available for next day delivery");
 
+        //add labels to the panel
         mainPanel.add(label);
         mainPanel.add(product);
         mainPanel.add(label2);
+        
         mainPanel.add(name);
         mainPanel.add(label3);
         mainPanel.add(itemList);
@@ -90,14 +106,25 @@ public class App extends JFrame {
         mainPanel.add(delivery);
 
         
-        /*mainPanel.add(new JTextField("Name",));
-        mainPanel.add(new JTextField("Product ID"));
-        mainPanel.add(new JTextField("Product ID"));
-        mainPanel.add(new JTextField("Product ID"));*/
+        //setting locations and sizes of the fields
+        mainPanel.setLayout(null);
+        label.setBounds(10, 10, 200, 100);
+        label2.setBounds(10, 50, 200, 100);
+        label3.setBounds(10, 90, 200, 100);
+        label4.setBounds(10,  130, 200, 100);
+
+        product.setBounds(150, 50, 200, 25);
+        name.setBounds(150, 90, 200, 25);
+        itemList.setBounds(150, 130, 200, 25);
+        quantity.setBounds(150,  170, 200, 25);
+        delivery.setBounds(10,210,200,100);
+        
 
         //Add ScrollBar
-        final JTextArea textArea = new JTextArea(26, 22);
+        rightPanel.setLayout(new GridLayout(1,0));
+        final JTextArea textArea = new JTextArea();
         scroll = new JScrollPane(textArea,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+ 
         rightPanel.add(scroll);
         
 
@@ -108,30 +135,39 @@ public class App extends JFrame {
         save.setText("Save");
         delete = new JButton();
         delete.setText("Delete Selected");
-        
+        delete.setEnabled(false);
+
+        //set locations of buttons
+        newItem.setBounds(130,10,100,20);
+        save.setBounds(240,10,70,20);
+        delete.setBounds(580,10,150,20);
+
+        bottomPanel.setLayout(null);
         bottomPanel.add(newItem);
         bottomPanel.add(save);
         bottomPanel.add(delete);
 
-        this.add(mainPanel,BorderLayout.CENTER);
-        this.add(rightPanel,BorderLayout.EAST);
-        this.add(bottomPanel,BorderLayout.SOUTH);
-
-
-        this.setVisible(true);     //Make frame visible
-
-    }
-
-    public class ProductListGUI{
         
+        //add the panels to the main frame
+        frame.add(mainPanel,BorderLayout.CENTER);
+        frame.add(rightPanel,BorderLayout.EAST);
+        frame.add(bottomPanel,BorderLayout.SOUTH);
+
+        
+
+
+        frame.setVisible(true);     //Make frame visible
+
     }
-
-
     
-    public static void main( String[] args )
-    {
-        new App();
+    public static void main( String[] args ){
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {;
+        public void run(){
+            ProductListGUI();
+        }
 
+
+        });
     }
         
         
---
+++ src/main/java/CS2020/assignment2/Product.java
@@ -0,0 +1,34 @@
+package CS2020.assignment2;
+
+public class Product {
+    String productId;
+    String type; 
+    int quantity;
+    String name;
+
+    //setter and getter methods for the 4 main variables
+    
+    public String getProductId(){
+        return productId;
+    }
+    public String getType(){
+        return type;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setProductId(String productId){
+        this.productId = productId;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    
+}
